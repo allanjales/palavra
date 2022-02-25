755e0e374fce8cdedfd663ab3b4fef898eac2f21
Allan Jales <jales.allan@gmail.com>
2022-02-25 16:12:44 -0300

    Sharing button fully supported + small bugs fixed

--- cssjs/main.js
+++ cssjs/main.js
@@ -1,8 +1,9 @@
 Game = function()
 {
-	this.target = null
+	this.target
 	this.start_date = new Date('2022-02-18 00:00:00')
-	this.db = null
+	this.diff_days
+	this.db
 
 	document.querySelector("body .holder").style.display = "none";
 
@@ -34,9 +35,9 @@ Game = function()
 			const uInt8Array = new Uint8Array(xhr.response)
 			this.db = new SQL.Database(uInt8Array)
 			
-			diff_days = Math.floor((new Date()-this.start_date) / (1000 * 60 * 60 * 24))
+			this.diff_days = Math.floor((new Date()-this.start_date) / (1000 * 60 * 60 * 24))
 			let length = this.db.exec('SELECT COUNT(*) FROM words_list')[0].values[0][0]
-			this.target = this.db.exec('SELECT word FROM words_list LIMIT 1 OFFSET $id', {$id: diff_days%length})[0].values[0][0]
+			this.target = this.db.exec('SELECT word FROM words_list LIMIT 1 OFFSET $id', {$id: this.diff_days%length})[0].values[0][0]
 			this.update_HUD()
 			document.querySelector("body .holder").style.display = "";
 			console.info('By Allan Jales')
@@ -177,8 +178,13 @@ Game = function()
 				for (let kbd of kbds)
 					if (kbd.innerText == spaces[j].innerText)
 					{
-						if (kbd.classList.contains("wrong") || kbd.classList.contains("right"))
+						if (kbd.classList.contains("right"))
 							break
+
+						if (kbd.classList.contains("wrong") && check_result[j] === "wrong")
+							break
+						else
+							kbd.classList.remove("wrong")
 
 						if (kbd.classList.contains("near"))
 						{
@@ -275,8 +281,13 @@ Game = function()
 			for (let kbd of kbds)
 				if (kbd.innerText == spaces[i].innerText)
 				{
-					if (kbd.classList.contains("wrong") || kbd.classList.contains("right"))
+					if (kbd.classList.contains("right"))
 						break
+
+					if (kbd.classList.contains("wrong") && check_result[i] === "wrong")
+						break
+					else
+						kbd.classList.remove("wrong")
 
 					let anim_name = "reveal_key"
 					if (kbd.classList.contains("near"))
@@ -384,6 +395,13 @@ Game = function()
 		switch_row(false)
 	}
 
+	this.is_finished = function()
+	{
+		if (document.querySelector(".edit_row>.letter_space"))
+			return false
+		return true
+	}
+
 	//Physical keyboard
 	document.addEventListener("keydown", (e) => this.KeyDown(e));
 
@@ -482,8 +500,8 @@ Game = function()
 					spaces[j].innerText = game.rows[i][j]
 			}
 
-			//If has finnished
-			if (!next_edit)
+			//If has finished
+			if (this.is_finished())
 				this.open_stats()
 		}
 	}
@@ -519,6 +537,12 @@ Game = function()
 				bars[i].style.width = this.stats.games[i]/max*100+"%"
 				bars[i].style.backgroundColor = ""
 			}
+
+		if (this.is_finished())
+			document.querySelector("#share_button").style.display = ""
+		else
+			document.querySelector("#share_button").style.display = "none"
+
 	}
 
 	this.update_timer = function()
@@ -557,19 +581,77 @@ Game = function()
 
 	this.share = function()
 	{
-		if (navigator.share) {
+		const rows = document.querySelectorAll("#board > .row");
+		let content = ""
+		let trys = "X"
+		for (i of rows.keys())
+		{
+			//Get row word
+			const spaces = rows[i].querySelectorAll(".letter_space")
+			let word = ""
+			for (const space of spaces)
+			{
+				if (space.innerText == "")
+					return
+				word += space.innerText
+			}
+
+			//Get check of word
+			let check_result = this.check_word(word)
+
+			//Add to content
+			content += "\n"
+			for (result of check_result)
+			{
+				switch (result)
+				{
+					case ("right"):
+						content += "🟩"
+						break
+					case ("near"):
+						content += "🟨"
+						break
+					case ("wrong"):
+						content += "⬛"
+						break
+					default:
+						content += "?"
+						break
+				}
+			}
+
+			if (check_result.every((val, i, arr) => val === "right"))
+				trys = i
+		}
+		let text = "joguei palavra! #"+this.diff_days+" "+trys+"/6\n"+content
+
+		if (!window.mobileAndTabletCheck && navigator.share)
+		{
 			navigator.share({
 				title: 'Palavra',
-				text: 'Hey, joguei Palavra!',
+				text: text,
 				url: 'https://allanjales.github.io/palavra',
 			})
 			.then(() => console.log('Successful share'))
-			.catch((error) => console.log('Error sharing', error));
+			.catch((error) => console.log('Error sharing', error))
+		}
+		else
+		{
+			navigator.clipboard.writeText(text+"\n\nallanjales.github.io/palavra")
+			.then(() => console.log('Async: Copying to clipboard was successful!'))
+			.catch((error) => console.log('Async: Could not copy text: ', error));
 		}
 	}
 
-	document.querySelector("#share_button").addEventListener("click", this.share, false);
+	document.querySelector("#share_button").addEventListener("click", (e) => this.share(), false);
 
 	this.load_cookie()
 }
+
+window.mobileAndTabletCheck = function() {
+	let check = false;
+	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+	return check;
+};
+
 Game()
